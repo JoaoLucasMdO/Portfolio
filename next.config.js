d7f3e19da0ac867e3d94e934d7af62b4a2880eee
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-05-08 11:43:15 -0300

    Att

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  // como você publicou em /Portfolio
-  basePath: isProd ? '/Portfolio' : '',
-  assetPrefix: isProd ? '/Portfolio/' : '',
-  trailingSlash: true,
-  // se quiser exportar tudo como estático:
-  output: 'export',
+const nextConfig = {
+  trailingSlash: false, 
 };
+
+module.exports = nextConfig;
