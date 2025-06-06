687880ff5d1b4266d7bd13d1f2a801ebbf0368c1
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2025-06-06 20:03:06 -0300

    Atualizações

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -6,16 +6,16 @@ export default function Header() {
       <nav className="container mx-auto px-4 py-4">
         <div className="flex justify-between items-center">
           {/* Logo */}
-          <a href="#inicio" className="text-white text-2xl font-bold hover:text-yellow-400">
+          <span className="text-white text-2xl font-bold ">
             João Lucas Melo de Oliveira
-          </a>
+          </span>
 
           {/* Menu Mobile */}
           <div className="md:hidden">
             <button
               data-target="mobile-demo"
               className="text-white text-2xl"
-              // Aqui você pode colocar a lógica para mostrar o menu em dispositivos móveis
+            // Aqui você pode colocar a lógica para mostrar o menu em dispositivos móveis
             >
               <i className="material-icons">menu</i>
             </button>
@@ -24,16 +24,16 @@ export default function Header() {
           {/* Menu Desktop */}
           <ul className="hidden md:flex space-x-8 text-white">
             <li>
-              <a href="#inicio" className="hover:text-yellow-400">Início</a>
+              <a href="#inicio" className="hover:text-cyan-300">Início</a>
             </li>
             <li>
-              <a href="#projetos" className="hover:text-yellow-400">Projetos</a>
+              <a href="#projetos" className="hover:text-cyan-300">Projetos</a>
             </li>
             <li>
-              <a href="#formacoes" className="hover:text-yellow-400">Formações</a>
+              <a href="#formacoes" className="hover:text-cyan-300">Formações</a>
             </li>
             <li>
-              <a href="#certificacoes" className="hover:text-yellow-400">Certificações</a>
+              <a href="#certificacoes" className="hover:text-cyan-300">Certificações</a>
             </li>
           </ul>
         </div>
